fc11ee7ea47aa495e803d313c6d454bba2035f5c
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-29 20:13:50 -0300

    Criando conexão com o banco de dados

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'sqlite3',
     connection: {
-      filename: './src/database/db.sqlite3'
+      filename: './src/database/db.sqlite'
     },
     migrations: {
       directory: './src/database/migrations'
