ecf2f1a2b4f5ce5b009295ca8a0259541d2b0726
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-30 15:41:46 -0300

    Adicionando migration user

--- backend/src/database/migrations/20200430151942_user.js
+++ backend/src/database/migrations/20200430151942_user.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('user', function(table) {
+        table.string('id').primary;
+        table.string('mail').notNullable();
+        table.string('password').notNullable();
+        table.string('name').notNullable();
+        table.string('phone').notNullable();
+        table.string('company').notNullable();
+    
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('user');
 };
