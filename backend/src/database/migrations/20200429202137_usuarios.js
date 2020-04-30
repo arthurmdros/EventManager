9bccce4752ff24327dd1863edb715fd4712ad87d
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-29 21:04:39 -0300

    Estruturando tabela usuarios

--- backend/src/database/migrations/20200429202137_usuarios.js
+++ backend/src/database/migrations/20200429202137_usuarios.js
@@ -1,8 +1,11 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('usuarios', function (table){
+        table.string('id').primary();
+        table.string('login').notNullable();
+        table.string('senha').notNullable();
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('usuarios');
 };
