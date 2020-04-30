d79556ec55fcb7953edc177cc9e43ba3f70eaf7b
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-29 22:27:55 -0300

    Adicionando rotas para o controlador de eventos

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const UsuarioController = require('./controllers/UsuarioController');
-
+const EventoController = require('./controllers/EventoController');
 
 const routes = express.Router();
 
@@ -8,5 +8,10 @@ routes.get('/users', UsuarioController.index);
 routes.post('/users', UsuarioController.create);
 routes.delete('/users/:id', UsuarioController.delete);
 routes.put('/users/:id', UsuarioController.update);
+
+routes.get('/events', EventoController.index);
+routes.post('/events', EventoController.create);
+routes.delete('/events/:id', EventoController.delete);
+routes.put('/events/:id', EventoController.update);
 
 module.exports = routes;
