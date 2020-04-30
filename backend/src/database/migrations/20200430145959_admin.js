4ad7d65bd18dc0abf154b3fd868eea984e4a4c3b
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-30 15:40:48 -0300

    Adicionando migration admin

--- backend/src/database/migrations/20200430145959_admin.js
+++ backend/src/database/migrations/20200430145959_admin.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('admin', function(table) {
+        table.string('id').primary;
+        table.string('login').notNullable();
+        table.string('password').notNullable();
+
+        table.string('comp_id').notNullable();
+
+        table.foreign('comp_id').references('id').inTable('company');
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('admin');
 };
