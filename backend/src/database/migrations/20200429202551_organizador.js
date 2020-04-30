7bc42ec578fdeaaf7b1c2b1327ea8e9eb4c0a37c
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-29 21:07:32 -0300

    Estruturando tabela organizador

--- backend/src/database/migrations/20200429202551_organizador.js
+++ backend/src/database/migrations/20200429202551_organizador.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('organizador', function (table) {
+        table.string('nome').notNullable();
+        table.string('email').notNullable();
+        table.string('telefone');
+        table.string('user_id').notNullable();
+
+        table.foreign('user_id').references('id').inTable('usuarios');
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('organizador');
 };
