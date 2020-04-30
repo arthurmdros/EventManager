3032c2813493e584c0e80cab3bcb6c8f394d33f9
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-30 16:11:57 -0300

    Adicionando rotas do  controlador admin

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,23 +1,11 @@
 const express = require('express');
-const UsuarioController = require('./controllers/UsuarioController');
-const EventoController = require('./controllers/EventoController');
-const OrgController = require('./controllers/OrganizadorController');
+const AdminController = require('./controllers/AdminController');
 
 const routes = express.Router();
 
-routes.get('/users', UsuarioController.index);
-routes.post('/users', UsuarioController.create);
-routes.delete('/users/:id', UsuarioController.delete);
-routes.put('/users/:id', UsuarioController.update);
-
-routes.get('/events', EventoController.index);
-routes.post('/events', EventoController.create);
-routes.delete('/events/:id', EventoController.delete);
-routes.put('/events/:id', EventoController.update);
-
-routes.get('/orgs', OrgController.index);
-routes.post('/orgs/:user_id', OrgController.create);
-routes.delete('/orgs/:user_id', OrgController.delete);
-routes.put('/orgs/:user_id', OrgController.update);
+routes.get('/admin/index', AdminController.index);
+routes.post('/admin/create', AdminController.create);
+routes.delete('/admin/update/:id', AdminController.delete);
+routes.put('/admin/delete/:id', AdminController.update);
 
 module.exports = routes;
