fc44dc2c45b5c180be6009864cbe427214716ea2
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-30 15:43:34 -0300

    Adicionando migration company

--- backend/src/database/migrations/20200430152244_company.js
+++ backend/src/database/migrations/20200430152244_company.js
@@ -1,8 +1,15 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('company', function (table) {
+        table.string('id').primary;
+        table.string('name').notNullable();
+        table.string('service').notNullable();        
+        table.string('mail').notNullable();
+        table.string('phone').notNullable();
+        
+    });
 };
 
+
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('company');
 };
