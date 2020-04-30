0d828a1f7ca95ea09e92106eae61e273f2352c45
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-30 15:44:20 -0300

    Adicionando migration ticket

--- backend/src/database/migrations/20200430152515_ticket.js
+++ backend/src/database/migrations/20200430152515_ticket.js
@@ -1,8 +1,14 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('ticket', function (table) {
+        table.string('id').primary;
+        table.string('type').notNullable();
+        table.decimal('value').notNullable();        
+        table.integer('amount').notNullable();
+        
+    });
 };
 
+
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('ticket');
 };
