7814708bd9aa3fe9bae27ef328be13898cf50af6
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-30 16:17:11 -0300

    Adicionando rotas para a nova tabela user

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const AdminController = require('./controllers/AdminController');
+const UserController = require('./controllers/UserController');
 
 const routes = express.Router();
 
@@ -7,5 +8,11 @@ routes.get('/admin/index', AdminController.index);
 routes.post('/admin/create', AdminController.create);
 routes.delete('/admin/update/:id', AdminController.delete);
 routes.put('/admin/delete/:id', AdminController.update);
+
+routes.get('/user/index', UserController.index);
+routes.post('/user/create', UserController.create);
+routes.put('/user/update/:id', UserController.update);
+routes.delete('/user/delete/:id', UserController.delete);
+
 
 module.exports = routes;
