ad603520fab103cc68b695d88fff2d6c6e078956
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-29 23:07:09 -0300

    Adicionando rotas para controlador de organizador

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const UsuarioController = require('./controllers/UsuarioController');
 const EventoController = require('./controllers/EventoController');
+const OrgController = require('./controllers/OrganizadorController');
 
 const routes = express.Router();
 
@@ -13,5 +14,10 @@ routes.get('/events', EventoController.index);
 routes.post('/events', EventoController.create);
 routes.delete('/events/:id', EventoController.delete);
 routes.put('/events/:id', EventoController.update);
+
+routes.get('/orgs', OrgController.index);
+routes.post('/orgs/:user_id', OrgController.create);
+routes.delete('/orgs/:user_id', OrgController.delete);
+routes.put('/orgs/:user_id', OrgController.update);
 
 module.exports = routes;
