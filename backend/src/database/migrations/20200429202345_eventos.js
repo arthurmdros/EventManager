083cf6379d696309957f4a1310b78cbb430e2262
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-29 21:05:47 -0300

    Estruturando tabela eventos

--- backend/src/database/migrations/20200429202345_eventos.js
+++ backend/src/database/migrations/20200429202345_eventos.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex) {
-  
+    return knex.schema.createTable('eventos', function (table) {
+        table.string('id').primary;
+        table.string('nome').notNullable();
+        table.date('data').notNullable();
+        table.string('horario').notNullable();
+        
+    });
 };
 
 exports.down = function(knex) {
-  
+    return knex.schema.dropTable('eventos');
 };
