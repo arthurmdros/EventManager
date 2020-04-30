43fba47b1e775073f7bf1ecdd0c3b15718cdbf51
arthurmdros <arthurmedeiros2005@gmail.com>
2020-04-30 16:10:50 -0300

    Adicionando controlador da tabela admin

---
+++ backend/src/controllers/AdminController.js
@@ -0,0 +1,45 @@
+const crypto = require('crypto');
+const connection = require('../database/connection');
+
+module.exports = {
+    async index (req,res) {
+        const admin = await connection('admin').select('*');
+
+        return res.json(admin);
+    }, 
+    
+    async create (req,res) {
+        const {login,password} = req.body;
+
+        const id = crypto.randomBytes(4).toString('Hex');
+
+        await connection('admin').insert({
+            id,
+            login,
+            password,            
+        });
+
+        return res.json({id});
+    },
+
+    async delete (req, res) {
+        const { id } = req.params;
+
+        await connection('admin').where('id', id).delete();
+
+        return res.json(204).send();
+    },
+
+    async update (req,res) {
+        const { id } = req.params;
+
+        const {password} = req.body;
+
+        await connection('admin').where('id', id)
+        .update({
+            password,
+        });
+
+        return res.json({id});
+    }
+}
