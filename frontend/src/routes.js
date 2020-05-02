a85353b4c29c657ea68fd812d1cc6c416ffec196
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-02 00:00:19 -0300

    Adicionando rota para página admin

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Admin from './pages/Admin';
 
 export default function Routes(){
     return(
         <BrowserRouter>
             <Switch>
                 <Route path="/page/login" exact component = {Login} />
+                <Route path="/page/admin" exact component = {Admin} />
             </Switch>
         </BrowserRouter>
     );
