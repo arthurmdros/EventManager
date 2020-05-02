57d739b1abff0fb1ab43e5c303c7cbae68df2c34
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-02 17:05:48 -0300

    Adicionando componentes da página

--- frontend/src/pages/Register/index.js
+++ frontend/src/pages/Register/index.js
@@ -1,8 +1,69 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import { Form } from '@unform/web';
+import { FiArrowLeft } from 'react-icons/fi';
+import logo from '../../assets/logo.png';
+
 import './styles.css';
+import Input from '../component/input';
 
 export default function Register(){
+    const formRef = useRef();
+
+    function navigateToLogin(){
+        alert("Página login");
+    }     
+    
+    function handleSubmit(data, {reset}){
+        console.log(data);
+        reset();
+    }
+
     return(
-        <p>Register page</p>
+        <div className="registerContainer">
+            <div className="content">
+                <section>
+                    <img src={logo} alt="Event Manager"/>
+
+                    <h1>Resgitro</h1>
+                    <p>Ao registrar-se no Event Manager você terá uma melhor organização, divulgação e credibilidade na realização de seus eventos.</p>
+                    <a onClick={navigateToLogin}>
+                        <FiArrowLeft size={16} color="#FFF"/>
+                        Já possuo registro                
+                    </a>
+                </section>            
+            <Form ref={formRef} onSubmit={handleSubmit}>
+                <h1>E-Mail:</h1>
+                <Input
+                    name="mail"
+                    type="mail"
+                />
+                <h1>Senha:</h1>
+                <Input
+                    name="password"
+                    type="password"
+                />
+                <h1>Nome:</h1>
+                <Input
+                    name="name"
+                    type="name"
+                />
+                <h1>Telefone:</h1>
+                <Input
+                    name="phone"
+                    type="phone"
+                />
+                <h1>Empresa:</h1>
+                <Input
+                    name="company"
+                    type="company"
+                />
+
+                <button className="btnForm" onPress={() => formRef.current.submitForm()}>
+                    Registar
+                </button>
+
+            </Form>
+            </div>
+        </div>
     );
 }
