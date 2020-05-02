5d5ba300a9d3ce4573e940a1ccee0b6f45826bfe
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-02 17:25:14 -0300

    Adicionando página profile de um usuário

---
+++ frontend/src/pages/Profile/index.js
@@ -0,0 +1,85 @@
+import React from 'react';
+import {BsPower, BsTrash} from "react-icons/bs";
+import {useHistory} from 'react-router-dom';
+
+import logo from '../../assets/logo.png';
+import './styles.css';
+
+export default function Profile(){
+    const navigation = useHistory();
+
+    function disparar(){
+        alert('Cadastrar novo evento');
+    }
+
+    function logout(){
+        navigation.push('/page/login');
+    }
+
+    function deleteEvent(){
+        alert('Deletar evento');
+    }
+
+    return(
+        <div className="profileContainer">
+            <header>
+                <img src={logo} alt="Event Manager"/>
+                <span>Bem vindo, Fulano de tal</span>
+                
+                <a onClick={disparar}>Cadastrar evento</a>
+                <button onClick={logout} type="button">
+                    <BsPower size={18} color="#E02041"/>                    
+                </button>
+            </header>
+
+            <h1>Eventos</h1>
+
+            <ul>
+                <li>
+                    <strong>Evento:</strong>
+                    <p>LiquidSky</p>
+
+                    <strong>Tipo evento:</strong>
+                    <p>Festa rave</p>
+
+                    <button onClick={deleteEvent} type="button">
+                        <BsTrash size={18} color="#1393f6"/>
+                    </button>
+                </li>   
+                <li>
+                    <strong>Evento:</strong>
+                    <p>LiquidSky</p>
+
+                    <strong>Tipo evento:</strong>
+                    <p>Festa rave</p>
+
+                    <button onClick={deleteEvent} type="button">
+                        <BsTrash size={18} color="#1393f6"/>
+                    </button>
+                </li> 
+                <li>
+                    <strong>Evento:</strong>
+                    <p>LiquidSky</p>
+
+                    <strong>Tipo evento:</strong>
+                    <p>Festa rave</p>
+
+                    <button onClick={deleteEvent} type="button">
+                        <BsTrash size={18} color="#1393f6"/>
+                    </button>
+                </li> 
+                <li>
+                    <strong>Evento:</strong>
+                    <p>LiquidSky</p>
+
+                    <strong>Tipo evento:</strong>
+                    <p>Festa rave</p>
+
+                    <button onClick={deleteEvent} type="button">
+                        <BsTrash size={18} color="#1393f6"/>
+                    </button>
+                </li>         
+            </ul>
+        </div>
+    );
+}
