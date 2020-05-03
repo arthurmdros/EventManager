c2efafc9d08174b5e9b50827862a3db97e6f39b2
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-03 16:43:35 -0300

    Atualizando componentes da página newCompany

--- frontend/src/pages/NewCompany/index.js
+++ frontend/src/pages/NewCompany/index.js
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useRef } from "react";
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import { Form } from "@unform/web";
+
+import './styles.css';
+import logo from '../../assets/logo.png';
+import Input from '../component/input';
 
 export default function NewCompany(){
+    const formRef = useRef();
+    const navigation = useHistory();
+
+    function handleSubmit(data, {reset}){
+        console.log(data);
+        reset();
+        navigation.push('/page/admin/profile');
+    }
+
     return(
-        <h1>New company</h1>
+        <div className="newCompany-container">
+            <div className="content">
+                <section>
+                    <img src={logo} alt="Event Manager"/>
+                    <h1>Cadastrar empresa</h1>
+                    <p>Cadastre empresas que possam contribuir nos eventos com seus serviços, facilitando o contrato entre prestadores de serviços e organizadores de eventos.</p>
+                    <Link to="/page/admin/profile">
+                        <FiArrowLeft size={16} color="#FFF"/>
+                        Voltar para home
+                    </Link>
+                </section>
+
+                <Form ref={formRef} onSubmit={handleSubmit}> 
+                    <h1>Empresa:</h1> 
+                    <Input 
+                        name="name"
+                        type="name"
+                    />
+                    <h1>Serviço:</h1>
+                    <Input 
+                        name="service"
+                        type="service"
+                    />
+                    <h1>E-mail:</h1>
+                    <Input 
+                        name="mail"
+                        type="mail"
+                    />
+                    <h1>Telefone:</h1>
+                    <Input 
+                        name="phone"
+                        type="phone"
+                    />
+                    <button className="btnForm" onPress={() => formRef.current.submitForm()}>
+                        Salvar
+                    </button>
+
+                </Form>
+            </div>
+        </div>
     );
 }
