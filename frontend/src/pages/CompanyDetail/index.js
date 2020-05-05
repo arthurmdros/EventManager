b15462ce770cc6b27a5d91c846309464e7028637
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-05 19:45:19 -0300

    Implementação de carregar item por meio do useLocation

--- frontend/src/pages/CompanyDetail/index.js
+++ frontend/src/pages/CompanyDetail/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {Link, useLocation} from "react-router-dom";
 import {FiArrowLeft, FiEdit} from 'react-icons/fi';
 import {BsTrash} from 'react-icons/bs';
 
@@ -7,6 +7,10 @@ import logo from "../../assets/logo.png";
 import './styles.css';
 
 export default function CompanyDetail(){
+    const route = useLocation();
+    const item = route.state;    
+    const admin_id = item.admin_id;
+        
     function deleteCompany(){
         alert('Deletar Empresa');        
     }
@@ -27,16 +31,16 @@ export default function CompanyDetail(){
                 <ul>
                     <li>
                         <strong>Empresa:</strong>
-                        <p>Unforte Vigilância</p>
+                        <p>{item.name}</p>
 
                         <strong>Serviço:</strong>
-                        <p>Segurança</p>
+                        <p>{item.service}</p>
 
                         <strong>E-mail:</strong>
-                        <p>unforte@mail.com</p>
+                        <p>{item.mail}</p>
 
                         <strong>Telefone:</strong>
-                        <p>(00) 1234-5678</p>
+                        <p>{item.phone}</p>
 
                         <Link to="/page/admin/company/update">
                             Atualizar informações 
