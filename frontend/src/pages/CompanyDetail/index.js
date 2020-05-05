5365890de4c3250471baf9f5a2cff0a06aa0c107
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-05 01:04:44 -0300

    Adicionando componentes na página company detail

--- frontend/src/pages/CompanyDetail/index.js
+++ frontend/src/pages/CompanyDetail/index.js
@@ -1,9 +1,53 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import {FiArrowLeft, FiEdit} from 'react-icons/fi';
+import {BsTrash} from 'react-icons/bs';
 
+import logo from "../../assets/logo.png";
 import './styles.css';
 
 export default function CompanyDetail(){
+    function deleteCompany(){
+        alert('Deletar Empresa');        
+    }
+
     return(
-        <h1>Company detail</h1>
+        <div className="company-container">
+            <div className="content">
+                <section>
+                    <img src={logo} alt="Event Manager"/>
+                    <h1>Informações</h1>
+                    <p>Visualize os dados comerciais da empresa, atualize informações e exclua caso necessário.</p>
+                    <Link to="/page/admin/profile">
+                        <FiArrowLeft size={16} color="#FFF"/>
+                        Voltar para home
+                    </Link>
+                </section>   
+
+                <ul>
+                    <li>
+                        <strong>Empresa:</strong>
+                        <p>Unforte Vigilância</p>
+
+                        <strong>Serviço:</strong>
+                        <p>Segurança</p>
+
+                        <strong>E-mail:</strong>
+                        <p>unforte@mail.com</p>
+
+                        <strong>Telefone:</strong>
+                        <p>(00) 1234-5678</p>
+
+                        <Link to="/page/admin/company/update">
+                            Atualizar informações 
+                            <FiEdit size={16} color="#1393f6"/>
+                        </Link>
+                        <button onClick={deleteCompany} type="button">
+                            <BsTrash size={18} color="#1393f6"/>
+                        </button>
+                    </li>
+                </ul>
+        </div>
+        </div>
     );
 }
