bcff63b2f0b488bbaba45b87b3030f401db9cacb
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-05 15:48:13 -0300

    Adicionando componentes na página event update

--- frontend/src/pages/EventUpdate/index.js
+++ frontend/src/pages/EventUpdate/index.js
@@ -1,9 +1,104 @@
-import React from "react";
+import React, { useRef, useState } from "react";
+import { Link, useHistory } from 'react-router-dom';
+import { FiArrowLeft } from 'react-icons/fi';
+import DatePicker from 'react-datepicker';
+import "react-datepicker/dist/react-datepicker.css";
+import { Form } from "@unform/web";
 
-import "./styles.css";
+import './styles.css';
+import logo from '../../assets/logo.png';
+import Input from '../component/input';
 
-export default function EventUpdate(){
+export default function NewEvent(){    
+    const [startDate, setStartDate] = useState(new Date());
+    const [endDate, setEndDate] = useState(new Date());
+    const [starTime, setStartTime] = useState(new Date());
+    const [endTime, setEndTime] = useState(new Date());
+    
+    const formRef = useRef();
+    const navigation = useHistory();
+
+    function handleSubmit(data, {reset}){
+        console.log(data);
+        reset();
+        navigation.push('/page/user/profile');        
+    }
+
     return(
-        <h1>Event Update</h1>
+        <div className="eventupdate-container">
+            <div className="content">
+                <section>
+                    <img src={logo} alt="Event Manager"/>
+                    <h1>Atualizar informações</h1>                    
+                    <Link to="/page/user/event/detail">
+                        <FiArrowLeft size={16} color="#FFF"/>
+                        Voltar
+                    </Link>
+                </section>
+
+                <Form ref={formRef} onSubmit={handleSubmit}> 
+                    <h1>Evento:</h1> 
+                    <Input 
+                        name="title"
+                        type="title"
+                    />
+                    <h1>Descrição:</h1>
+                    <Input 
+                        name="description"
+                        type="description"
+                    />
+                    <section className="titles">
+                        <h1>Inicio ás:</h1>
+                        <h1>Termina ás:</h1>
+                    </section>
+                    <div className = "container-time">                        
+                        <DatePicker
+                            selected={starTime}
+                            onChange={date => setStartTime(date)}
+                            showTimeSelect
+                            showTimeSelectOnly                               
+                            timeIntervals={30}
+                            timeCaption="Time"
+                            dateFormat="h:mm a"                                
+                            
+                        />                        
+                        <DatePicker
+                            selected={endTime}
+                            onChange={date => setEndTime(date)}
+                            showTimeSelect
+                            showTimeSelectOnly                            
+                            timeIntervals={30}
+                            timeCaption="Time"
+                            dateFormat="h:mm a"                            
+                        />
+                    </div>
+                    <section className="titles">                        
+                        <h1>Inicio dia:</h1>
+                        <h1>Termina dia:</h1>
+                    </section>
+                    <div className= "container-date">                        
+                        <DatePicker
+                            selected={startDate}
+                            onChange={date => setStartDate(date)}
+                            dateFormat="dd/MM/yyyy"                                                        
+                        />                        
+                        <DatePicker
+                            selected={endDate}
+                            onChange={date => setEndDate(date)}
+                            dateFormat="dd/MM/yyyy"
+                        />
+                    </div>
+                    <h1>Tipo de evento:</h1>
+                    <Input 
+                        name="event"
+                        type="event"
+                    />
+                    <button className="btnForm" onPress={() => formRef.current.submitForm()}>
+                        Salvar
+                    </button>
+
+                </Form>
+            </div>
+        </div>
     );
 }
