155e77da23e6d27630cd958d1ff69e2c72ba73e7
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-05 18:01:14 -0300

    Implementação de carregar o perfil admin da api

--- frontend/src/pages/Admin/index.js
+++ frontend/src/pages/Admin/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React,{useState, useEffect} from 'react';
 import {BsPower, BsTrash} from "react-icons/bs";
 import {FiArrowRight} from "react-icons/fi";
 import {Link, useHistory} from 'react-router-dom';
 
+import api from '../../services/api';
 import logo from '../../assets/logo.png';
 import './styles.css';
 
 export default function Admin(){
+    const [companies, setCompanies] = useState([]);
+    const admin_id = localStorage.getItem('admin_id');
+    const login = localStorage.getItem('login');
+    
     const navigation = useHistory();    
     
+    useEffect(() =>{
+        api.get('admin/profile',{
+            headers: {
+                Authorization: admin_id,
+            }
+        }).then(response => {
+            setCompanies(response.data);
+        })
+    }, [admin_id]);
+
     function logout(){
+        localStorage.clear();
         navigation.push('/page/login');
     }
 
@@ -21,7 +37,7 @@ export default function Admin(){
         <div className="adminContainer">
             <header>
                 <img src={logo} alt="Event Manager"/>
-                <span>Bem vindo, Administrador</span>
+                <span>Bem vindo, {login}</span>
                 <div>
                     <Link to="/page/admin/newcompany">Cadatrar empresa</Link>                    
                     <Link to="/page/admin/update">Atualizar senha</Link>
@@ -34,108 +50,25 @@ export default function Admin(){
             <h1>Empresas</h1>
 
             <ul>
-                <li>
-                    <strong>Empresa:</strong>
-                    <p>Unforte Vigilância</p>
+                {companies.map(company => (
+                    <li key={company.id}>
+                        <strong>Empresa:</strong>
+                        <p>{company.name}</p>
 
-                    <strong>Serviço:</strong>
-                    <p>Segurança</p>
+                        <strong>Serviço:</strong>
+                        <p>{company.service}</p>
 
-                    <Link to="/page/admin/company/detail">
-                        Ver detalhes
-                        <FiArrowRight size={18} color="#1393f6"/>
-                    </Link>
+                        <Link to="/page/admin/company/detail">
+                            Ver detalhes
+                            <FiArrowRight size={18} color="#1393f6"/>
+                        </Link>
 
-                    <button onClick={deleteCompany} type="button">
-                        <BsTrash size={18} color="#1393f6"/>
-                    </button>
+                        <button onClick={deleteCompany} type="button">
+                            <BsTrash size={18} color="#1393f6"/>
+                        </button>
 
-                </li>   
-                <li>
-                    <strong>Empresa:</strong>
-                    <p>Unforte Vigilância</p>
-
-                    <strong>Serviço:</strong>
-                    <p>Segurança</p>
-
-                    <Link to="/page/admin/company/detail">
-                        Ver detalhes
-                        <FiArrowRight size={18} color="#1393f6"/>
-                    </Link>
-
-                    <button onClick={deleteCompany} type="button">
-                        <BsTrash size={18} color="#1393f6"/>
-                    </button>
-
-                </li> 
-                <li>
-                    <strong>Empresa:</strong>
-                    <p>Unforte Vigilância</p>
-
-                    <strong>Serviço:</strong>
-                    <p>Segurança</p>
-
-                    <Link to="/page/admin/company/detail">
-                        Ver detalhes
-                        <FiArrowRight size={18} color="#1393f6"/>
-                    </Link>
-
-                    <button onClick={deleteCompany} type="button">
-                        <BsTrash size={18} color="#1393f6"/>
-                    </button>
-
-                </li> 
-                <li>
-                    <strong>Empresa:</strong>
-                    <p>Unforte Vigilância</p>
-
-                    <strong>Serviço:</strong>
-                    <p>Segurança</p>
-
-                    <Link to="/page/admin/company/detail">
-                        Ver detalhes
-                        <FiArrowRight size={18} color="#1393f6"/>
-                    </Link>
-
-                    <button onClick={deleteCompany} type="button">
-                        <BsTrash size={18} color="#1393f6"/>
-                    </button>
-
-                </li> 
-                <li>
-                    <strong>Empresa:</strong>
-                    <p>Unforte Vigilância</p>
-
-                    <strong>Serviço:</strong>
-                    <p>Segurança</p>                  
-
-                    <button onClick={deleteCompany} type="button">
-                        <BsTrash size={18} color="#1393f6"/>
-                    </button>
-                    
-                    <Link to="/page/admin/company/detail">
-                        Ver detalhes
-                        <FiArrowRight size={18} color="#1393f6"/>
-                    </Link>
-
-                </li> 
-                <li>
-                    <strong>Empresa:</strong>
-                    <p>Unforte Vigilância</p>
-
-                    <strong>Serviço:</strong>
-                    <p>Segurança</p>
-
-                    <Link to="/page/admin/company/detail">
-                        Ver detalhes
-                        <FiArrowRight size={18} color="#1393f6"/>
-                    </Link>
-
-                    <button onClick={deleteCompany} type="button">
-                        <BsTrash size={18} color="#1393f6"/>
-                    </button>
-
-                </li>                     
+                    </li>                                        
+                ))}                
             </ul>
         </div>
     );
