8de784f27eec67f382604672bc6c686c22ecf844
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-06 00:49:23 -0300

    Implementação de deletar empresa pelo company detail

--- frontend/src/pages/CompanyDetail/index.js
+++ frontend/src/pages/CompanyDetail/index.js
@@ -1,18 +1,29 @@
 import React from "react";
-import {Link, useLocation} from "react-router-dom";
+import {Link, useLocation, useHistory} from "react-router-dom";
 import {FiArrowLeft, FiEdit} from 'react-icons/fi';
 import {BsTrash} from 'react-icons/bs';
 
+import api from '../../services/api';
 import logo from "../../assets/logo.png";
 import './styles.css';
 
 export default function CompanyDetail(){
+    const navigation = useHistory();
     const route = useLocation();
     const item = route.state;    
     const admin_id = item.admin_id;
         
-    function deleteCompany(){
-        alert('Deletar Empresa');        
+    async function deleteCompany(id){
+        try{
+            await api.delete(`/company/delete/${id}`, {
+                headers:{
+                    Authorization: admin_id,
+                }
+            })                
+            navigation.push('/page/admin/profile');
+            }catch(err){
+                alert('Erro ao deletar empresa, tente novamente.');
+            }
     }
 
     return(
@@ -46,7 +57,7 @@ export default function CompanyDetail(){
                             Atualizar informações 
                             <FiEdit size={16} color="#1393f6"/>
                         </Link>
-                        <button onClick={deleteCompany} type="button">
+                        <button onClick={() => deleteCompany(item.id)} type="button">
                             <BsTrash size={18} color="#1393f6"/>
                         </button>
                     </li>
