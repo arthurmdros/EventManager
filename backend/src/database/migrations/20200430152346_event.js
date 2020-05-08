890e17c5dc69982df84d7a2cfabd463946dd2355
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-08 18:00:15 -0300

    Atualizando campos da tabela

--- backend/src/database/migrations/20200430152346_event.js
+++ backend/src/database/migrations/20200430152346_event.js
@@ -3,11 +3,11 @@ exports.up = function(knex) {
         table.string('id').primary;
         table.string('title').notNullable();
         table.string('description').notNullable();
-        table.time('start_time', { precision: 6 }).notNullable();
-        table.time('end_time', { precision: 6 }).notNullable();
-        table.date('start_date').notNullable();
-        table.date('end_date').notNullable();
-        table.string('event').notNullable();
+        table.string('selectedStartTime').notNullable();
+        table.string('selectedEndTime').notNullable();
+        table.string('selectedStartDate').notNullable();
+        table.string('selectedEndDate').notNullable();
+        table.string('selectedValue').notNullable();
         
         table.string('user_id').notNullable();
                 
