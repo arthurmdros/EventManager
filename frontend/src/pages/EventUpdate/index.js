78a84ae0f3ac197199f795de0db366c2f7743f19
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-08 23:58:24 -0300

    Implementação de atualizar um evento

--- frontend/src/pages/EventUpdate/index.js
+++ frontend/src/pages/EventUpdate/index.js
@@ -1,27 +1,108 @@
-import React, { useRef, useState } from "react";
-import { Link, useHistory } from 'react-router-dom';
+import React, { useState } from "react";
+import { useHistory, useLocation } from 'react-router-dom';
 import { FiArrowLeft } from 'react-icons/fi';
 import DatePicker from 'react-datepicker';
 import "react-datepicker/dist/react-datepicker.css";
-import { Form } from "@unform/web";
+import Select from 'react-select';
 
+import api from '../../services/api';
 import './styles.css';
 import logo from '../../assets/logo.png';
-import Input from '../component/input';
+
 
 export default function NewEvent(){    
-    const [startDate, setStartDate] = useState(new Date());
-    const [endDate, setEndDate] = useState(new Date());
-    const [starTime, setStartTime] = useState(new Date());
-    const [endTime, setEndTime] = useState(new Date());
+    const navigation = useHistory();    
+    const route = useLocation();
+    const item = route.state;      
+    const user_id = item.user_id; 
     
-    const formRef = useRef(null);
-    const navigation = useHistory();
+    const [title, setTitle] = useState('');
+    const [description, setDescription] = useState('');
+    const [startDate, setStartDate] = useState(new Date());
+    const [selectedStartDate, setSelectedStartDate] = useState('');   
 
-    function handleSubmit(data, {reset}){
-        console.log(data);
-        reset();
-        navigation.push('/page/user/profile');        
+    const [endDate, setEndDate] = useState(new Date());    
+    const [selectedEndDate, setSelectedEndDate] = useState('');  
+
+    const [startTime, setStartTime] = useState(new Date());    
+    const [selectedStartTime, setSelectedStartTime] = useState(''); 
+
+    const [endTime, setEndTime] = useState(new Date());    
+    const [selectedEndTime, setSelectedEndTime] = useState('');   
+
+    const eventoptions = [
+        { value: 'Show', label: 'Show' },
+        { value: 'Festival ou Feira', label: 'Festival ou Feira' },
+        { value: 'Viagem, Retiro, Acampamento ou Excursão', label: 'Viagem, Retiro, Acampamento ou Excursão' },
+        { value: 'Festa ou Evento social', label: 'Festa ou Evento social' },
+        { value: 'Seminário ou Palestra', label: 'Seminário ou Palestra' },
+        { value: 'Convenção', label: 'Convenção' },
+        { value: 'Aula, Treinamento ou Workshop', label: 'Aula, Treinamento ou Workshop' }
+      ]    
+    const [selectedValue, setSelectedValue] = useState('');
+
+
+    async function updateEvent(e){
+        e.preventDefault();
+        const data = { 
+            title,
+            description,
+            selectedStartDate,            
+            selectedEndDate,
+            selectedStartTime,
+            selectedEndTime,
+            selectedValue
+        };
+        
+        try {
+            await api.put(`event/update/${item.id}`, data, {
+                headers:{
+                    Authorization: user_id,
+                }
+            })
+            alert('Atualizado com sucesso!');
+            navigation.push('/page/user/profile');
+        }catch(err){
+            alert('Erro ao atualizar, tente novamente.');
+        }
+    }
+
+    function navigateToDetail(item){
+        navigation.push('/page/user/event/detail', item);
+    }
+  
+    function handleChange (e){
+        setSelectedValue(e.value);
+    }
+
+    function hanldeStartDateChange(date) {  
+        setStartDate(date);
+        const day = date.getDate();   
+        const month = date.getMonth() + 1; 
+        const year = date.getFullYear();   
+        setSelectedStartDate(day+'/'+month+'/'+year);        
+    }
+
+    function hanldeEndDateChange(date) {  
+        setEndDate(date);
+        const day = date.getDate();   
+        const month = date.getMonth() + 1; 
+        const year = date.getFullYear();   
+        setSelectedEndDate(day+'/'+month+'/'+year);        
+    }
+
+    function hanldeStartTimeChange(date) {  
+        setStartTime(date);
+        const hour = date.getHours();   
+        const minute = date.getMinutes();         
+        setSelectedStartTime(hour+':'+minute);                
+    }
+
+    function hanldeEndTimeChange(date) {  
+        setEndTime(date);
+        const hour = date.getHours();   
+        const minute = date.getMinutes();         
+        setSelectedEndTime(hour+':'+minute);     
     }
 
     return(
@@ -29,23 +110,30 @@ export default function NewEvent(){
             <div className="content">
                 <section>
                     <img src={logo} alt="Event Manager"/>
-                    <h1>Atualizar informações</h1>                    
-                    <Link to="/page/user/event/detail">
+                    <h1>Atualizar informações</h1>  
+                    <button className="detail-link" onClick={() => navigateToDetail(item)} type="button">
                         <FiArrowLeft size={16} color="#FFF"/>
                         Voltar
-                    </Link>
+                    </button>   
                 </section>
 
-                <Form ref={formRef} onSubmit={handleSubmit}> 
-                    <h1>Evento:</h1> 
-                    <Input 
-                        name="title"
-                        type="title"
+                <form onSubmit={updateEvent}>
+                    <h1>Evento:</h1>
+                    <input                         
+                        value={title}
+                        onChange={e => setTitle(e.target.value)}
                     />
                     <h1>Descrição:</h1>
-                    <Input 
-                        name="description"
-                        type="description"
+                    <textarea                         
+                        value={description}
+                        onChange={e => setDescription(e.target.value)}
+                    />
+                    <h1>Tipo de evento:</h1>
+                    <Select                         
+                        className="select-input" 
+                        options={eventoptions}
+                        value={eventoptions.filter(obj => obj.value === selectedValue)} 
+                        onChange={handleChange}
                     />
                     <section className="titles">
                         <h1>Inicio ás:</h1>
@@ -53,23 +141,25 @@ export default function NewEvent(){
                     </section>
                     <div className = "container-time">                        
                         <DatePicker
-                            selected={starTime}
-                            onChange={date => setStartTime(date)}
+                            selected={startTime}
+                            onChange={date => hanldeStartTimeChange(date)}
                             showTimeSelect
                             showTimeSelectOnly                               
                             timeIntervals={30}
                             timeCaption="Time"
                             dateFormat="h:mm a"                                
+                            value={startTime}
                             
                         />                        
                         <DatePicker
                             selected={endTime}
-                            onChange={date => setEndTime(date)}
+                            onChange={date => hanldeEndTimeChange(date)}
                             showTimeSelect
                             showTimeSelectOnly                            
                             timeIntervals={30}
                             timeCaption="Time"
-                            dateFormat="h:mm a"                            
+                            dateFormat="h:mm a"  
+                            value={endTime}                          
                         />
                     </div>
                     <section className="titles">                        
@@ -79,25 +169,19 @@ export default function NewEvent(){
                     <div className= "container-date">                        
                         <DatePicker
                             selected={startDate}
-                            onChange={date => setStartDate(date)}
-                            dateFormat="dd/MM/yyyy"                                                        
+                            onChange={date => hanldeStartDateChange(date)}                         
+                            dateFormat="dd/MM/yyyy"  
+                            value={startDate}                                                         
                         />                        
                         <DatePicker
                             selected={endDate}
-                            onChange={date => setEndDate(date)}
+                            onChange={date => hanldeEndDateChange(date)}
                             dateFormat="dd/MM/yyyy"
+                            value={endDate}    
                         />
-                    </div>
-                    <h1>Tipo de evento:</h1>
-                    <Input 
-                        name="event"
-                        type="event"
-                    />
-                    <button className="btnForm" onPress={() => formRef.current.submitForm()}>
-                        Salvar
-                    </button>
-
-                </Form>
+                    </div>                    
+                    <button className="btnForm" type="submit">Cadastrar</button>
+                </form>
             </div>
         </div>
     );
