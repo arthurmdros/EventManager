2710e0add2636fe7441887896d78046a8bb8e483
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-13 17:03:43 -0300

    Selecionar uma empresa na página dos detalhes

--- frontend/src/pages/CompanyUser/index.js
+++ frontend/src/pages/CompanyUser/index.js
@@ -1,15 +1,29 @@
 import React from "react";
-import {Link, useLocation} from "react-router-dom";
+import {Link, useLocation, useHistory} from "react-router-dom";
 import {FiArrowLeft, FiCheckCircle} from 'react-icons/fi';
 
+import api from '../../services/api';
 import logo from "../../assets/logo.png";
 import './styles.css';
 
 export default function CompanyDetail(){
    
+    const navigation = useHistory();
     const route = useLocation();
-    const item = route.state;    
-     
+    const item = route.state;   
+    const event_id = localStorage.getItem('event_id');
+
+    async function selectCompany(){    
+        item.event_id = event_id;
+        try{
+            await api.put(`company/select/${item.id}`, item);                 
+            alert('Empresa selecionada com sucesso.');
+            navigation.push('/page/user/newevent/company');
+        }catch(err){
+            alert('Erro ao selecionar empresa, tente novamente.');
+        }        
+    }
+    
 
     return(
         <div className="company-user">
@@ -37,7 +51,7 @@ export default function CompanyDetail(){
                         <strong>Telefone:</strong>
                         <p>{item.phone}</p>
 
-                        <button className="button-select" onClick={() => alert('Emrpesa selecionada.')} type="button">
+                        <button className="button-select" onClick={selectCompany} type="button">
                             <FiCheckCircle size={18} color="#1393f6"/>
                         </button>
                     </li>
