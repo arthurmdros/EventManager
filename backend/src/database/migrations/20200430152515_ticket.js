cacc8e32208f17a94a32ea834b150a7206921751
arthurmdros <arthurmedeiros2005@gmail.com>
2020-05-13 16:59:05 -0300

    Atualizando chave primária da tabela ticket

--- backend/src/database/migrations/20200430152515_ticket.js
+++ backend/src/database/migrations/20200430152515_ticket.js
@@ -1,6 +1,6 @@
 exports.up = function(knex) {
     return knex.schema.createTable('ticket', function (table) {
-        table.string('id').primary;
+        table.increments();
         table.string('type').notNullable();
         table.decimal('value').notNullable();        
         table.integer('amount').notNullable();
