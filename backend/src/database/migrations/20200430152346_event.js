25afdd4bcbfcebfccb923a2f3c7f581b1663a575
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-13 18:19:42 -0300

    Adicionando novas colunas dentro da tabela event

--- backend/src/database/migrations/20200430152346_event.js
+++ backend/src/database/migrations/20200430152346_event.js
@@ -1,13 +1,18 @@
 exports.up = function(knex) {
     return knex.schema.createTable('event', function (table) {
         table.string('id').primary;
+        table.string('image').notNullable();
         table.string('title').notNullable();
         table.string('description').notNullable();
         table.string('selectedStartTime').notNullable();
         table.string('selectedEndTime').notNullable();
         table.string('selectedStartDate').notNullable();
         table.string('selectedEndDate').notNullable();
-        table.string('selectedValue').notNullable();
+        table.string('selectedValue').notNullable();        
+        table.decimal('latitude').notNullable();
+        table.decimal('longitude').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
         
         table.string('user_id').notNullable();
                 
