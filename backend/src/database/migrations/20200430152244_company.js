4a55f6a99cccccbe27a79618883835c11e5545a5
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-13 18:19:28 -0300

    Adicionando novas colunas dentro da tabela company

--- backend/src/database/migrations/20200430152244_company.js
+++ backend/src/database/migrations/20200430152244_company.js
@@ -1,10 +1,15 @@
 exports.up = function(knex) {
     return knex.schema.createTable('company', function (table) {
         table.string('id').primary;
+        table.string('image').notNullable();
         table.string('name').notNullable();
         table.string('service').notNullable();        
         table.string('mail').notNullable();
-        table.string('phone').notNullable();
+        table.string('phone').notNullable();        
+        table.decimal('latitude').notNullable();
+        table.decimal('longitude').notNullable();
+        table.string('city').notNullable();
+        table.string('uf', 2).notNullable();
 
         table.string('admin_id').notNullable();
         table.string('event_id');
