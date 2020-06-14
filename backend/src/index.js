44b4f8e625aa5b741a26a8b9d7a5d3181eda5a11
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-14 00:10:40 -0300

    Adicionando envio de erros padrão do celebrate

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const path = require('path');
 const routes = require('./routes');
+const classCelebrate = require('celebrate');
 
 const app = express();
 
@@ -10,6 +11,8 @@ app.use(express.json());
 app.use(routes);
 
 app.use('/uploads', express.static(path.resolve(__dirname, '..','uploads')));
+
+app.use(classCelebrate.errors());
 
 app.listen(3333);
 
