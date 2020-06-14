907eee9af5e184e4462b61fd1848c7814b971bf7
arthurmdros <arthurmedeiros2005@gmail.com>
2020-06-13 23:42:49 -0300

    Adicionando rota de acesso aos uploads de eventos

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const routes = require('./routes');
 
 const app = express();
@@ -7,6 +8,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+
+app.use('/uploads', express.static(path.resolve(__dirname, '..','uploads')));
 
 app.listen(3333);
 
